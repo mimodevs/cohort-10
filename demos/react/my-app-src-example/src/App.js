e7be2f35704ea0a801aec01a4afe4d5f4937e970
twistedstack <kelly@getmimo.com>
2022-09-28 20:24:47 -0400

    Just quick example of react router
    Just quick example of react router
    https://www.w3schools.com/react/react_router.asp

---
+++ demos/react/my-app-src-example/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Layout from './pages/Layout';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
+
+
+
